b76090c16ecbe96e9a0762e54597affa72cf3e44
sammysam254 <Sammyseth260@gmail.com>
2025-10-19 22:52:35 +0300

    Add components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-4">
+      <div className="container mx-auto text-center">
+        &copy; 2024 Handmade Crafts
+      </div>
+    </footer>
+  );
+}
